595826f1e32f8c626f1942c540b7a118e1b771c8
Deepak Chittora <chittoradee@gmail.com>
2022-01-06 10:31:16 +0530

    added switch, exact and nested routes

---
+++ src/pages/Products.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Products = () => {
+	return (
+		<section>
+			<h1>The Products Page</h1>
+			<ul>
+				<li>
+					<Link to="/products/a-book">A Book</Link>
+				</li>
+				<li>
+					<Link to="/products/a-carpet">A Carpet</Link>
+				</li>
+				<li>
+					<Link to="/products/an-online-course">An Online Course</Link>
+				</li>
+			</ul>
+		</section>
+	);
+};
+export default Products;
